9c93bbfd10691aa3fe75e2296166cd4d4ce42090
Dimitrilhostis <103448862+Dimitrilhostis@users.noreply.github.com>
2024-11-15 23:10:41 +0100

    Add start command to package.json

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/events', require('./routes/eventRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
 
 // Lancer le serveur
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
@@ -46,10 +46,3 @@ admin.messaging().send(message).then(response => {
 }).catch(error => {
   console.log('Erreur d\'envoi de notification', error);
 });
-
-
-// Connection à mongoDB
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connecté à MongoDB'))
-  .catch(err => console.log('Erreur MongoDB:', err));
